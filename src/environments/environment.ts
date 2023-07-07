f8f160fdf80cc0cdfd247a0ce0a452e05558b9e3
zolcin <marizolflores@hotmail.com>
2023-07-07 15:48:07 -0600

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const enviroment={
+    production: false,
+    firestore: {
+        apiKey: "AIzaSyDTaFS71PW9Hs_XUazAOHqlBUrha1AehF8",
+        authDomain: "control-clientes-52a92.firebaseapp.com",
+        projectId: "control-clientes-52a92",
+        storageBucket: "control-clientes-52a92.appspot.com",
+        messagingSenderId: "61779891034",
+        appId: "1:61779891034:web:f3df9aa1d275443f4dd963"
+    }
+};
